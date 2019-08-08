958b2a39f07a98233f8e3722fe35fe2c5b90cb1f
victorrborges <victor.araujo@ccc.ufcg.edu.br>
2019-08-08 20:31:49 -0300

    Migrating database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_08_08_193242) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
-    t.integer "todo_list_id"
+    t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "completed_at"
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_08_08_193242) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
